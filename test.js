4091f16f9258933831249d81f17910e54ae61fcd
johnwcallahan <johnwcallahan@gmail.com>
2018-03-21 07:49:21 -0700

    add test case for hyphen

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 let punctuate = require("./index");
 
 console.log(punctuate("hello comma world exclamation point it's a beautiful day period how are you question mark"))
-console.log(punctuate("what sort of time \\period are you interested in learning about question mark"));
+console.log(punctuate("hey there dash what sort of time \\period are you interested in learning about question mark"));
